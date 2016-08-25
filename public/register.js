890e59ad88322c63700368ab5a05d6e98b0d3f43
sergey <sergey.troyanchuk@gmail.com>
2016-08-25 15:51:08 +0300

    worked version for register with _underscore templating

--- public/register.js
+++ public/register.js
@@ -2,53 +2,57 @@ $(function(){
 
 	/** dom links --------------- */
 
-		var registerCustomerForm = document.getElementById('registerCustomerForm');
+		
 		var post_data = document.getElementById('post_data');
-		var sendAjaxForm = document.querySelector('.sendAjaxForm');
+		var register_customer_form_template = document.getElementById('register_customer_form_template');
+		var user_from_server_template = document.getElementById('user_from_server_template');
+		var send_ajax_form = document.querySelector('.send_ajax_form');
 		var navbar = document.querySelector('.navbar');
 
 	/** classes --------------- */
 	
 		var User = Backbone.Model.extend({
-			initialize: function(){
-	           console.log('user model created');
-	       	},
 	       	defaults: {
-	           first_name: 'no first name',
-	            last_name: 'no last name',
+	       			   id: 'not set',
+	           first_name: 'new in model  first name',
+	            last_name: 'new in model no last name'
+	   			// country: req.body.country,
+				// vehicle1: req.body.vehicle1,
+				// vehicle2: req.body.vehicle2,
+				// gender: req.body.gender
+	       	},
+	       	initialize: function(){
+	            console.log('user model initialized');
+
+				this.on('change', function(){
+					//if(this.hasChanged('first_name')){
+						console.log('- Values for this model have changed - trigger from base class, first name');
+						userData.render();
+					//}
+				});
+
+				this.on("invalid", function(model, error){
+					console.log(error);
+				});
+	       	},
+	       	validate: function(some_attributes){
+	       		if(some_attributes.hasOwnProperty('first_name')){
+	       			some_attributes.first_name = some_attributes.first_name.trim();
+	       			var is_number = /^[\d\s]+$/;
+	       			if(is_number.test(some_attributes.first_name)){
+	       				return 'no number are allowed!';	
+	       			}
+	       		}
 	       	}
 		});
 
-	/** models --------------- */
-
 		var user = new User({});
 
-		/** add events to user model */
-		_.extend(user, Backbone.Events);
 
+	/** common func --------------- */	
 
-
-		user.on("change", function(msg) {
-			if(user.hasChanged(['first_name'])){
-				changeUserView();
-				console.log(user.previous("first_name")+' vs '+user.get('first_name')) ;
-			}
-		  	console.log("user changed - "+msg);
-		});
-
-	/** func --------------- */
-		var changeUserView = function(){
-
-			var first_name = user.get('first_name');
-			var last_name = user.get('last_name');
-
-			post_data.innerHTML =  first_name + '<br>' + last_name;
-		};	
-
-	/** events --------------- */
-
-		$(sendAjaxForm).on('click', function(){
-			var data = $(registerCustomerForm).serialize();
+		var registerAjax = function($dom, success){
+			var data = $dom.serialize();
 
 			$.ajax({
 				method: 'POST',
@@ -59,15 +63,7 @@ $(function(){
 					
 					var server_data = result.data;
 
-					if(result.status){
-						user.set({ 
-							first_name: server_data.first_name, 
-							last_name: server_data.last_name
-						});
-
-						/** trigger event if need to do it directly */
-						//user.trigger('userChange');
-					}
+					success(server_data);
 				},
 				error: function (jqXHR, textStatus, errorThrown) {
 			    	console.log(jqXHR);
@@ -75,6 +71,106 @@ $(function(){
 			    	console.log(errorThrown);
 			    }
 			});
+		};
+
+	/** views --------------- */
+
+		var RegisterCustomerForm = Backbone.View.extend({
+			initialize: function(){
+		        this.render();
+		    },
+			el: '#registerCustomerForm',
+			events: {
+				'click .send_ajax_form': 'sendReciveAjax'
+			},
+			template: _.template($(register_customer_form_template).html()),
+			model: user,
+			sendReciveAjax: function(){
+				var self = this;
+				var success = function(server_data){
+					self.model.set({
+						id: server_data.id,
+						first_name: server_data.first_name, 
+						last_name: server_data.last_name,
+						country: server_data.country,
+						vehicle1: server_data.vehicle1,
+						vehicle2: server_data.vehicle2,
+						gender: server_data.gender
+					});	
+				}
+
+				registerAjax(this.$el, success);
+			},
+			render: function(){
+				this.$el.html(this.template({data:this.model.attributes}));
+				return this;		
+			}
 		});
 
+		var registerUser = new RegisterCustomerForm({});
+
+		var UserDataList = Backbone.View.extend({
+			el: '#post_data',
+			template: _.template($(user_from_server_template).html()),
+			model: user,
+			render: function(){
+				this.$el.html(this.template({d:this.model.attributes}));
+				return this;
+			}
+		});
+
+		var userData = new UserDataList();
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var foundUser = function(id){
+	var found = false;
+	for(var i=0; i<users.length; i++){
+		if(users[i].id === id){
+			found = true;
+			break;
+		}
+	}
+	return found;
+};
+
 
 var app = express();
 
@@ -11,6 +22,8 @@ app.use(bodyParser.urlencoded({
 
 app.use('/public', express.static(__dirname + '/public'));
 app.use('/frameworks', express.static(__dirname + '/frameworks'));
+
+var users = [];
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -23,11 +36,20 @@ app.get('/register', function (req, res) {
 app.post('/register', function(req, res){
 
 	var post_data = {
-		country: req.body.country_value,
-		state: req.body.state_value,
+		id: users.length,
 		first_name: req.body.first_name,
-		last_name: req.body.last_name
+		last_name: req.body.last_name,
+		country: req.body.country,
+		vehicle1: req.body.vehicle1,
+		vehicle2: req.body.vehicle2,
+		gender: req.body.gender
 	};
+
+	if(req.body.id && foundUser(req.body.id)){
+		users[req.body.id] = post_data;
+	}else{
+		users.push(post_data);
+	}
 
 	var response = {
 	    status  : true,
@@ -35,6 +57,8 @@ app.post('/register', function(req, res){
 	};
 
 	res.json(response);
+
+	console.log(users);
 
 });
 
